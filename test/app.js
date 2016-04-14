a0949125d5b93025ab441ad799c84c5fdd18510d
ing. P.T. Kroon <tijmen.kroon@ciber.nl>
2016-04-14 14:01:05 +0200

    excluded hooks from pre-test

--- test/app.js
+++ test/app.js
@@ -1,18 +1,18 @@
 'use strict';
 // var path = require('path');
-var assert = require('yeoman-assert');
+// var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-ciberionic:app', function () {
-   before(function (done) {
+  before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({someAnswer: true})
       .on('end', done);
-   });
+  });
 
-  //it('creates files', function () {
+  // it('creates files', function () {
   //  assert.file([
   //    '../generators/app/index.js'
   //  ]);
-  //});
+  // });
 });
